d0d514703871dab1503dc87f6710651996cfe506
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-09-04 06:42:23 +0530

    feat: poll uncomitted file

--- haste-frontend/pages/server/[name].tsx
+++ haste-frontend/pages/server/[name].tsx
@@ -7,29 +7,48 @@ import { getAPI, getHeaders } from '../../utils/getAPI'
 const ServerData = () => {
 	const { isReady, query } = useRouter()
 
-	let call_made = false
-
 	const getLogs = async () => {
-		if (call_made === false) {
-			call_made = true
+		// if (call_made === false) {
+		// call_made = true
 
-			const url = getAPI('logs')
+		const url = getAPI('logs')
 
-			url.searchParams.set('server_id', query.name as string)
-			const processes = await fetch(url, {
-				headers: getHeaders(),
-			})
-			const data: { errors: string; logs: string } = await processes.json()
+		url.searchParams.set('server_id', query.name as string)
 
-			setLogData(data)
+		if (logData?.total_errors_len) {
+			url.searchParams.set('error_from_line', logData.total_errors_len.toString())
 		}
+
+		if (logData?.total_logs_len) {
+			url.searchParams.set('log_from_line', logData.total_logs_len.toString())
+		}
+
+		const processes = await fetch(url, {
+			headers: getHeaders(),
+		})
+		const data: { errors: string; logs: string; total_logs_len: number; total_errors_len: number } =
+			await processes.json()
+
+		setLogData((_logData) => ({
+			...data,
+			logs: (_logData?.logs ?? '') + data.logs,
+			errors: (_logData?.errors ?? '') + data.errors,
+		}))
 	}
 
-	const [logData, setLogData] = useState<{ errors: string; logs: string }>()
+	const [logData, setLogData] = useState<{
+		errors: string
+		logs: string
+		total_errors_len: number
+		total_logs_len: number
+	}>()
+	const [poll, setPoll] = useState<number>(-1)
 
 	useEffect(() => {
 		if (isReady && query.name) getLogs()
 	}, [isReady])
+
+	const [, setCounter] = useState(0)
 
 	const logRef = useRef<HTMLTextAreaElement>(null)
 	const errorRef = useRef<HTMLTextAreaElement>(null)
@@ -41,7 +60,21 @@ const ServerData = () => {
 		if (logData?.errors && errorRef.current) {
 			errorRef.current.scrollTop = errorRef.current.scrollHeight
 		}
-	}, [logData])
+
+		if (isReady && logData?.logs !== undefined && logData?.errors !== undefined) {
+			if (poll === -1) {
+				console.log('polling')
+
+				const poll_interval = setInterval(() => {
+					setCounter((counter) => counter + 1)
+					getLogs()
+				}, 5_000)
+
+				setPoll(poll_interval as any)
+			}
+		}
+	}, [logData, poll, isReady])
+
 	return (
 		<>
 			{
