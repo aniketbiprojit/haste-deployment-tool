db2455b0c01688002418537c8773d94f42d53eaa
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-09-04 18:35:47 +0530

    fix: build

--- haste-frontend/pages/_app.tsx
+++ haste-frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { AuthState, useAuth } from './hooks/useAuth'
+import { AuthState, useAuth } from '../hooks/useAuth'
 import { Authorize } from '../components/Authorize'
 
 function HasteApplication({ Component, pageProps }: AppProps) {
